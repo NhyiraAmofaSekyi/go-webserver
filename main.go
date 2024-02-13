636211ad2086c8a82a1c3e9ceb0026338194773b
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2024-02-13 17:23:44 +0200

    Read write timeout

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	v1 "github.com/NhyiraAmofaSekyi/go-webserver/internal/v1"
 	"github.com/NhyiraAmofaSekyi/go-webserver/utils"
@@ -21,6 +22,11 @@ func main() {
 		Handler: corsEnabledMux, // Your wrapped handler
 		Addr:    ":8080",        // Listen address
 		// Other configurations like ReadTimeout, WriteTimeout, etc.
+		ReadTimeout:       5 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       120 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		MaxHeaderBytes:    1 << 20,
 	}
 
 	err := srv.ListenAndServe()
