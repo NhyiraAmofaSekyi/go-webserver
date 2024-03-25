4aa308bb601448bed1cd8751b24a630760db8209
NhyiraAmofaSekyi <nhyiraamofasekyi@gmail.com>
2024-03-25 11:04:06 +0200

    gracefull shutdown

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	v1 "github.com/NhyiraAmofaSekyi/go-webserver/internal/v1"
@@ -18,7 +23,7 @@ func main() {
 
 	corsEnabledMux := utils.CorsWrapper(mainMux)
 
-	srv := &http.Server{
+	server := &http.Server{
 		Handler: corsEnabledMux, // wrapped handler
 		Addr:    ":8080",        // Listen address
 		// Other configurations like ReadTimeout, WriteTimeout, etc.
@@ -28,10 +33,32 @@ func main() {
 		ReadHeaderTimeout: 5 * time.Second,
 		MaxHeaderBytes:    1 << 20,
 	}
+	// Create a channel to listen for interrupt signals.
+	quit := make(chan os.Signal, 1)
+	// Register the given channel to receive notifications of the specified signals.
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
-	err := srv.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
+	// Start the server in a goroutine.
+	go func() {
+		fmt.Println("Server goroutine starting...")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("Error starting server: %v\n", err)
+		}
+	}()
+
+	// Block until a signal is received.
+	<-quit
+	fmt.Println("Shutting down server...")
+
+	// Create a context with a timeout for the shutdown process.
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// Attempt to gracefully shut down the server.
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Errorf("server shutdown failed: %v", err)
 	}
+
+	fmt.Println("Server gracefully stopped.")
 
 }
